e9f10306ae79d15d4a02165103f157024ffcbfcc
uniq47 <unique.sapkota01@student.csulb.edu>
2023-01-05 08:30:41 -0800

    Initial react component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//file name with capital letter is a component
+
+// function App(){
+//     return <div>Hello</div>
+//   }
+
+//we need to export to import, use default keyword to export
+// export default App;-> when we have only one export we use default keyword
+
+//named export
+export function App(){
+    return <div>Hello</div>
+  }
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import {App} from './App';
 
 //ReatDOM.render(element, document.getElementById("root"))
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>
-    <h1>Hello</h1>
-    </div>
-);
+//reactroot to create a React root for displaying content inside a browser DOM element.
+//when its not accepting any parameter we self close the parameter
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
 
