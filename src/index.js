299857c889db21032d826955d8431e0bd50048e8
uniq47 <unique.sapkota01@student.csulb.edu>
2023-01-04 17:23:22 -0800

    initial create-reat-app setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+//ReatDOM.render(element, document.getElementById("root"))
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Hello</h1>
+    </div>
+);
+
