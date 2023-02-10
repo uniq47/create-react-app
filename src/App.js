dcd94edd2fbf531d355ea951ec7fbd92ab8b144c
uniq47 <unique.sapkota01@student.csulb.edu>
2023-02-10 10:30:48 -0800

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import React, { useState, useEffect } from "react";
-//we will not return anything from useEffect hook like we do in a normal function or usestate hook
-//we will pass a fucntion inside the useEffect hook as an argument
-//we did i in map, filter, it's called callback function
-//it will be called later when something happens or it will be called later
-// itself by react, it is managed by useeffect hook
-//1) first it woll fun the jsx code and then it will run the useEffect hook
-// it will run after everything after the component is completed
-//  it will re run when the componenent will rerender(calling the function twice)
-// when the state is changed,during the process also ti will effect at the end
-// cant use set count  inside the as it is infinite loop
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
   console.log("before useEffect");
 
-  //useEffect( function () {},[]) will run at theend ,
-  //[] is the dependency array, it will run only once, when we want to
-  //[count] the value count is changing so it will go in the use eddect hook, once itwill use useEffect hook
-  //we cna pass another argument to the useEffect hook
+  console.log(user);
+  console.log(user?.title)
   useEffect(() => {
     console.log("inaise use effect");
-  });
-  // we are going to pase some data from external apo, that the popular use case of useEffect hook hook
-  //  we interact with data from external api (server)
-  //
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setUser(json));
+  }, []);
+
   console.log("after useEffect");
-  return (
-    <div>
-      {console.log("jsx")}
-      <h1>hello</h1>
-    </div>
-  );
+     //check if there is user, if yes then display the title else display loading
+  return <div>{user?.title || <h1>loading...</h1>}</div>;
+  // user? user.title : <h1>Loading...</h1>
+  //if user not defined 
+  //user?.title: <h1>Loading...</h1> means that if user is defined then only check for title else display loading
 }
 
 export default App;
 
-// main thing of a useeffect hook is to create side effects
+
+
+//optional channing operator is used to check if the object is defined or not
